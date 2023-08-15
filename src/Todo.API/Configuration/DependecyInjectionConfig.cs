0fedf8a603eb4ef4f9cfd288ebb0fe19f503aae2
augusto <augustosaaude@gmail.com>
2023-08-14 21:05:05 -0300

    feat: add auth with identity

---
+++ src/Todo.API/Configuration/DependecyInjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using Todo.Core.Interfaces;
+using Todo.Core.Notifications;
+
+namespace Todo.API.Configuration;
+
+public static class DependecyInjectionConfig
+{
+    public static void ResolveDependecies(this IServiceCollection services)
+    {
+        services.AddScoped<INotificator, Notificator>();
+    }
+}
