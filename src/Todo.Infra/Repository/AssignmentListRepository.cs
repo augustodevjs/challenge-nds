479782972f1f0bd09f424a28a67c53887fe1a328
augusto <augustosaaude@gmail.com>
2023-08-15 20:33:54 -0300

    feat: create skeleton of Models in Repository

---
+++ src/Todo.Infra/Repository/AssignmentListRepository.cs
@@ -0,0 +1,11 @@
+﻿using Todo.Domain.Models;
+using Todo.Infra.Context;
+
+namespace Todo.Infra.Repository;
+
+public class AssignmentListRepository : Repository<AssignmentList>
+{
+    public AssignmentListRepository(TodoDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/Todo.Infra/Repository/AssignmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Todo.Domain.Models;
+using Todo.Infra.Context;
+
+namespace Todo.Infra.Repository;
+
+public class AssignmentRepository : Repository<Assignment>
+{
+    public AssignmentRepository(TodoDbContext context) : base(context)
+    {
+    }
+}
