859b157e1b064283b00c7574b2ba8954899d43bb
augusto <augustosaaude@gmail.com>
2023-08-20 20:30:11 -0300

    feat: add class of filter and pagiantion

---
+++ src/Todo.Infra/Filter/AssignmentFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo.Infra.Filter;
+
+public class AssignmentFilter
+{
+    public string Description { get; set; }
+    public DateTime? StartDeadline { get; set; }
+    public DateTime? EndDeadline { get; set; }
+    public bool? Concluded { get; set; }
+    public string OrderBy { get; set; } = "description";
+    public string OrderDir { get; set; } = "asc";
+}
---
+++ src/Todo.Infra/Paged/PagedResult.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Domain.Models;
+using Todo.Infra.Contracts;
+
+namespace Todo.Infra.Paged;
+
+public class PagedResult<T> : IPagedResult<T> where T : Entity, new()
+{
+    public List<T> Items { get; set; } = new();
+    public int Total { get; set; }
+    public int Page { get; set; }
+    public int PerPage { get; set; }
+    public int PageCount { get; set; }
+}
